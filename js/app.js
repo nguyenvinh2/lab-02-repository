b091485ba7b525d9a406b0cd58e019aeadacdfb1
Vinh Nguyen <nguyenv2@outlook.com>
2018-09-13 15:16:45 -0700

    sorting completed

--- js/app.js
+++ js/app.js
@@ -1,86 +1,122 @@
 'use strict';
 
-function Horn(hornObject) {
+function Horn(hornObject, page) {
 	this.title = hornObject.title;
 	this.description = hornObject.description;
 	this.image = hornObject.image_url;
 	this.keyword = hornObject.keyword;
 	this.hornNumber = hornObject.horns;
+	this.page = page;
 }
 
-Horn.prototype.render = function (section) {
-	this.section = section;
+Horn.prototype.render = function () {
 	const $source = $('#photo-template').html();
 	const template = Handlebars.compile($source);
 	return template(this);
 }
 
+Horn.allHorns = [];
 const hornKeyword = [];
 const pageData = ['data/page-1.json', 'data/page-2.json'];
 const sectionData = ['photos-page-1', 'photos-page-2'];
-let count = 0;
+let keepPageTrack = 'photos-page-1';
 
 Horn.readJSON = (page, section) => {
-	Horn.allHorns = [];
-	console.log(section)
-	$.get(page, 'json')
-		.then(data => {
-			data.forEach(animal => {
-				Horn.allHorns.push(new Horn(animal));
-			})
-			console.log(Horn.allHorns);
-			Horn.allHorns.forEach(animal => {
-				if (hornKeyword.indexOf(animal.keyword) === -1) {
-					hornKeyword.push(animal.keyword);
+	let asyncTiming = 0;
+	for (let i = 0; i < page.length; i++) {
+		$.get(page[i], 'json')
+			.then(data => {
+				asyncTiming++;
+				data.forEach(animal => {
+					Horn.allHorns.push(new Horn(animal, section[i]));
+				})
+				Horn.allHorns.forEach(animal => {
+					if (hornKeyword.indexOf(animal.keyword) === -1) {
+						hornKeyword.push(animal.keyword);
+					}
+				});
+				if (asyncTiming === page.length) {
+					sortByHorn;
+					Horn.loadHorns();
 				}
 			});
-			console.log(hornKeyword);
-			populateFilterList(hornKeyword);
-		})
-		.then(() => Horn.loadHorns(section));
+	}
 }
 
-Horn.loadHorns = (section) => {
-	Horn.allHorns.forEach(animal => $('#photos').append(animal.render(section)));
+Horn.loadHorns = () => {
+	populateFilterList(hornKeyword);
+	Horn.allHorns.forEach(animal => $('#photos').append(animal.render()));
+	$('.photos-page-2').hide();
+}
+
+const sortByHorn = () => {
+	Horn.allHorns.sort(function (HornOne, HornTwo) {
+		return HornOne.hornNumber - HornTwo.hornNumber;
+	});
+}
+
+const sortByName = () => {
+	Horn.allHorns.sort(function (NameOne, NameTwo) {
+		if (NameOne.title > NameTwo.title) {
+			return 1;
+		} else if (NameOne.title < NameTwo.title) {
+			return -1;
+		} else {
+			return 0
+		};
+	});
 }
 
 const populateFilterList = hornKeyword => {
-	$.each(hornKeyword, (index, value) => {
-		console.log(value);
+	$.each(hornKeyword.sort(), (index, value) => {
 		$('select').append(`<option value="${value}">${value.charAt(0).toUpperCase() + value.slice(1)}</option>`);
 	});
 }
 
 $('#first').click(() => {
+	$('select').get(0).selectedIndex = 0;
+	keepPageTrack = 'photos-page-1';
 	$('.photos-page-2').hide();
 	$('.photos-page-1').show();
-
 }
 );
 $('#second').click(() => {
+	$('select').get(0).selectedIndex = 0;
+	keepPageTrack = 'photos-page-2';
+	$('.photos-page-2').show();
 	$('.photos-page-1').hide();
-	if (count === 0) {
-		Horn.readJSON(pageData[1], sectionData[1]);
-		count++;
-	} else {
-		$('.photos-page-2').show();
-	}
-
-});
-
+}
+);
+$('#sort-name').click(() => {
+	$('main').empty();
+	sortByName();
+	Horn.loadHorns();
+	$('section').hide();
+	$(`.${keepPageTrack}`).show();
+}
+);
+$('#sort-horn').click(() => {
+	$('main').empty();
+	sortByHorn();
+	Horn.loadHorns();
+	$('section').hide();
+	$(`.${keepPageTrack}`).show();
+}
+);
 
 $('select').change(() => {
+	$('section').hide();
 	let $selection = $('select').val();
 
 	if ($selection === 'default') {
 		$('section').show();
 	} else {
 		$('section').hide();
-		$(`section[class="${$selection}"]`).show();
+		$(`.${$selection}.${keepPageTrack}`).show();
 	}
 
 })
 
 $(() => {
-	Horn.readJSON(pageData[0], sectionData[0]);
+	Horn.readJSON(pageData, sectionData);
 })
